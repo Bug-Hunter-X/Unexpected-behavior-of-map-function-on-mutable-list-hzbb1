4395a4d29066d953b3dd315d1808e73090d0b8ba
BugHunter-X <askzenai@gmail.com>
2025-01-23 07:12:05 +0100

    Add bug.kt file

---
+++ bug.kt
@@ -0,0 +1,9 @@
+fun main() {
+    val list = listOf(1, 2, 3, 4, 5)
+    val result = list.map { it * 2 }
+    println(result) // Output: [2, 4, 6, 8, 10]
+
+    val mutableList = mutableListOf(1, 2, 3, 4, 5)
+    mutableList.map { it * 2 }
+    println(mutableList) // Output: [1, 2, 3, 4, 5] //This is the unexpected behavior
+}
